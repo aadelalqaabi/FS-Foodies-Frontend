db8d4beecc348f8960f61fcc228764f5bb60a945
aa0241 <77753344+aa0241@users.noreply.github.com>
2022-06-09 03:48:50 +0300

    Add categories list and item

--- foodies/src/App.js
+++ foodies/src/App.js
@@ -1,15 +1,16 @@
-
 import "./App.css";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Nav from "./Components/Nav";
-
+import CategoriesList from "./Components/CategoriesList";
 
 function App() {
   return (
     <div>
       <Nav />
-    
+      <Routes>
+        <Route path="/CategoriesList" element={<CategoriesList />}></Route>
+      </Routes>
     </div>
   );
 }
